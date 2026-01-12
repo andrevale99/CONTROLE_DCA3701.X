96bb92e7013b441bd48bcf305bc33080a43905d0
andrevale99 <andrefvale@gmail.com>
2026-01-12 14:42:13 -0300

    Exemplo do Livro do Ogata

--- Estudos/SeguidorDeReferencia.py
+++ Estudos/SeguidorDeReferencia.py
@@ -52,22 +52,82 @@ def Exemplo_1_ServossistemaTIPO_1():
     plt.grid()
     plt.show()
     
-def Exemplo_2_ServossistemaTIPO_1(_R,_L):
+def Exemplo_2_ServossistemaTIPO_1(Polos=None):
     '''
-    Exemplo de projeto de um servossistema do tipo 1
-    quando nao ha integrador na planta. Usa como exemplo
-    um controle de corrente de um circuito RL (circuito presente
-    em motores de corrente continua).
+    Exemplo 10.5 do livro de Ogata "Engenharia de Controle Moderno".
 
-    Paramêtros:
-    _R : resistência do circuito RL (Ohms)
-    _L : indutância do circuito RL (Henrys)
+    Parametros do pêndulo invertido:
+    Polos: Posições dos 5 polos desejados
     '''
-    R = _R  # Ohms
-    L = _L  # Henrys
 
+    A = np.array([
+        [0,1,0,0],
+        [20.601, 0, 0, 0],
+        [0,0,0,1],
+        [-0.4905, 0, 0, 0]
+        ])
     
+    B = np.array([[0],[-1],[0],[0.5]])
+
+    C = np.array([[0,0,1,0]])
+
+    # Expandindo as matrizes para incluir o integrador
+    Ahat = np.concatenate((A, np.zeros((4,1))), axis=1)
+    temp = np.concatenate((-C, np.array([[0]])), axis=1)
+    Ahat = np.concatenate((Ahat, temp), axis=0)
+
+    Bhat = np.concatenate((B, np.array([[0]])), axis=0)
+
+    del temp
+
+    print("Matriz Ahat:\n", Ahat)
+    print("Matriz Bhat:\n", Bhat)
+
+    if Polos is None:
+        Polos = [-1+1j*np.sqrt(3), -1-1j*np.sqrt(3),
+                -5, -5, -5]
+        
+    K = ct.acker(Ahat, Bhat, Polos)
+    print(K)
+
+    KI = K[len(K)-1]
+    K = np.copy(K[0:len(K)-1]).reshape(1,4)
+    
+    print("Ganho de realimentação de estado K:\n", K)
+    print("Ganho de realimentação de estado KI:\n", KI)
+
+    # Aplicando no sistema realimentado
+    A00 = A - (B * K)
+    A01 = B * -KI
+    A10 = -C
+    A11 = np.array([[0]])
+    
+    Af = np.concatenate((A00, A01), axis=1)
+    temp = np.concatenate((A10, A11), axis=1)
+    Af = np.concatenate((Af, temp), axis=0)
+
+    print(Af)
+
+    del temp
+
+    Bf = np.array([[0],[0],[0],[0],[1]])
+    Cf = np.concatenate((C, np.array([[0]])), axis=1)
+    Df = np.array([[0]])
+
+    print("Matriz Af:\n", Af)
+    print("Matriz Bf:\n", Bf)
+    print("Matriz Cf:\n", Cf)
+    print("Matriz Df:\n", Df)
+
+    # Resposta do sistema realimentado
+    t, y = sg.step(sg.StateSpace(Af, Bf, Cf, Df))
+    plt.plot(t, y)
+    plt.title("Resposta ao Degrau do Servossistema Tipo 1 - Pêndulo Invertido")
+    plt.xlabel("Tempo (s)")
+    plt.ylabel("Saída posição")
+    plt.grid()
+    plt.show()
 
 if __name__ == "__main__":
     # Exemplo_1_ServossistemaTIPO_1()
-    Exemplo_2_ServossistemaTIPO_1(1, 0.5)
+    Exemplo_2_ServossistemaTIPO_1()
